532f4be6d0987f62cc73791dbeeec1ab5a00de19
Fhilippe Gabriel <fhilippegabriel15@gmail.com>
2021-12-02 20:55:51 -0300

    :up:

---
+++ routes/api.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+Route::middleware('auth:api')->group(function () {
+
+    Route::get("user", [UserController::class, "user"]);
+
+    Route::resource('post', PostController::class);
+
+});
+
+// Route::group(['middleware' => ['auth:sanctum']], function () {
+//     Route::get('/me', function (Request $request) {
+//         return auth()->user();
+//     });
+//     Route::post('/auth/logout', [AuthController::class, 'logout']);
+// });
+
+// // When the route is not defined
+// Route::get('/{any}', function () {
+//     return response()->json([
+//         'status' => 'Error',
+//         'message' => "The route is not defind!",
+//     ], 404);
+// })->where('any', '.*')->name('notFound');
+
+// Route::post('/{any}', function () {
+//     return response()->json([
+//         'status' => 'Error',
+//         'message' => "The route is not defind!",
+//     ], 404);
+// })->where('any', '.*')->name('notFound');
